b10e399902f98627e39a1dde68e1234f48d9c6f9
Morteza <morteza.ahmadianmanzary@gmail.com>
2026-06-26 23:03:41 +0200

    fixing themToggle

--- my-app/components/layout/themeToggle.tsx
+++ my-app/components/layout/themeToggle.tsx
@@ -1,21 +1,20 @@
-'use client'
-import { useTheme } from "next-themes";
+"use client"
+import { useTheme } from "next-themes"
 import { Sun, Moon } from "lucide-react";
-import { useEffect, useState } from "react";
-
+import {useState, useEffect} from "react"
 export default function ThemeToggle() {
-  const { theme, setTheme } = useTheme()
-  const [isLoaded, setIsLoaded] = useState<boolean>(false)
+    const { theme, setTheme } = useTheme()
+    const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
- // editor error
-  useEffect(() => {
-    setIsLoaded(true)
-  }, [])
+    useEffect(() => { setIsLoaded(true); }, []);
 
-  if (!isLoaded) return null
-  return (
-    <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")} className="cursor-pointer" >
-      {theme === "dark" ? <Sun className="text-white" /> : <Moon className="text-black" />}
-    </button>
-  )
-}
+    if (!isLoaded) { return null; }
+
+    return (
+        <button
+            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+            className="cursor-pointer"
+        >
+            {theme === "dark" ? <Sun className="text-white" /> : <Moon className="text-black" />}
+        </button>
+    ) }
