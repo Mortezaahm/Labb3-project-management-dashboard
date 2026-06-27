269021aa7bd976449d9af30784d0b9deeac52d52
Morteza <morteza.ahmadianmanzary@gmail.com>
2026-06-27 12:59:44 +0200

    Change the db name

--- my-app/components/layout/navbar.tsx
+++ my-app/components/layout/navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { LoginStatus } from "../loginStatus";
 // import ThemeToggle from "@/components/layout/ThemeToggle";
 import { ui } from "@/lib/styles"
-import ThemeToggle from "@/components/layout/ThemeToggle";
+import ThemeToggle from "@/components/layout/themeToggle";
 
 export default async function Navbar() {
 
