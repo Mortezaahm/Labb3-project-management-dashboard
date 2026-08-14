be9605bc3574130072307aea49c4887591d2e33a
Morteza <morteza.ahmadianmanzary@gmail.com>
2026-08-14 15:19:26 +0200

    fixing mobile menu font color

--- my-app/components/dashboard/DashboardHeader.tsx
+++ my-app/components/dashboard/DashboardHeader.tsx
@@ -5,8 +5,8 @@ type DashboardHeaderProps = {
 export default function DashboardHeader({userName}: DashboardHeaderProps) {
   return (
     <div>
-      <h1 className="text-2xl font-bold mb-4">Welcome {userName}</h1>
-      <p className="text-gray-600">Welcome to your dashboard! Here you can get an overview of your activities, recent projects, and important statistics at a glance.</p>
+      <h1 className="text-2xl font-bold mb-4 dark:text-white">Welcome {userName}</h1>
+      <p className="text-gray-600 dark:text-gray-300">Welcome to your dashboard! Here you can get an overview of your activities, recent projects, and important statistics at a glance.</p>
     </div>
   )
 }
